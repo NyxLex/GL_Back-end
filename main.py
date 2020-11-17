e1dbb65be593ca33d1714553b3efa2a3df46fc11
NyxLex <55287736+NyxLex@users.noreply.github.com>
2020-11-17 18:30:14 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+
+app = Flask("__name__")
+
+@app.route("/api/v1/hello-world-{13}")
+def index():
+    return "Hello World-13"
+
+
+if __name__ == '__main__':
+    app.run()
